cda19d89e9e2c02b5c73b6676fb3893fa6809c82
wanggb <wanggb@gdmbase.com>
2024-07-31 11:19:02 +0800

    docs(deps): 修改czconfig

--- .cz-config.js
+++ .cz-config.js
@@ -38,11 +38,11 @@ module.exports = {
 
   // 交互提示信息
   messages: {
-    type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
+    type: '确保本次提交遵循 commit 规范！\n选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
     // 选择 scope: custom 时会出下面的提示
     customScope: '请输入自定义的 scope：',
-    subject: '填写简短精炼的变更描述：\n',
+    subject: '填写简短的变更描述：\n',
     body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
     breaking: '列举非兼容性重大的变更（可选）：\n',
     footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
@@ -58,6 +58,4 @@ module.exports = {
   // subject 限制长度
   subjectLimit: 100,
   breaklineChar: '|', // 支持 body 和 footer
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true,
 };

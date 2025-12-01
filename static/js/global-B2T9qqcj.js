4c1390df99d99c65eb59972d29659b4b8b7f77bf
wanggb <wanggb@gdmbase.com>
2025-12-01 11:47:31 +0800

    deploy

--- static/js/global-B2T9qqcj.js
+++
@@ -1,1 +0,0 @@
-import{aw as e}from"./core-B_x7lh11.js";const t=e("config",{state:()=>({appDark:!1}),persist:{enabled:!0,strategies:[{storage:localStorage}]}});export{t as u};
---
+++ static/js/global-Ie0pEOwl.js
@@ -0,0 +1,1 @@
+import{aw as e}from"./core-DMyUAmhm.js";const t=e("config",{state:()=>({appDark:!1}),persist:{enabled:!0,strategies:[{storage:localStorage}]}});export{t as u};

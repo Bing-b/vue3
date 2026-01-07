f9601dca016022d9b9c61f666474f50cefd83f08
Bing-b <2606536469@qq.com>
2026-01-07 15:28:32 +0800

    fix(other): 修改提交流程

--- .cz-config.js
+++ .cz-config.js
@@ -35,7 +35,7 @@ module.exports = {
     customScope: '情输入修改的范围(可选)',
     subject: '请简要描述提交(必填)',
     body: '请输入详细描述(可选)',
-    footer: '请输入要关闭的issus(可选)',
+    // footer: '请输入要关闭的issus(可选)',
     confirmCommit: '确认要使用以上信息提交？(y/n)',
   },
 

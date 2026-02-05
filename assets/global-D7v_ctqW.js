a01b2802ddd24d663d0ce9f6e5154a1c31a40af7
wanggb <wanggb@gdmbase.com>
2026-02-05 17:37:59 +0800

    deploy

---
+++ assets/global-D7v_ctqW.js
@@ -0,0 +1,1 @@
+import{Zt as defineStore}from"./index-CidyMYlX.js";var global_default=defineStore(`config`,{state:()=>({appDark:!1}),persist:{enabled:!0,strategies:[{storage:localStorage}]}});export{global_default as t};
